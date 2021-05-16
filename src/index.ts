cd7d56d6265ba68d6d6385d5f08bcef9c25f002e
Aydar Akhmetov <aydarkin@gmail.com>
2021-05-16 15:21:51 +0300

    Resolved with points!

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ function randomInt(range: number) {
 }
 
 let i = 0
-let n = 5  // Имитируемое число объектов.
+let n = 1_000  // Имитируемое число объектов.
 let palette = ['#FF00FF', '#800080', '#FF0000', '#800000', '#FFFF00', '#00FF00', '#008000', '#00FFFF', '#0000FF', '#000080']
-let plotWidth = 800
-let plotHeight = 400
+let plotWidth = 32_000
+let plotHeight = 16_000
 
 // Пример итерирующей функции. Итерации имитируются случайными выдачами. Почти также работает режим демо-данных.
 function readNextObject() {
@@ -52,16 +52,12 @@ let scatterPlot = new SPlot('canvas1')
 // Настройка экземпляра на режим вывода отладочной информации в консоль браузера.
 // Другие примеры работы описаны в файле splot.js со строки 214.
 scatterPlot.setup({
-  iterationCallback: readNextObject2,
+  iterationCallback: readNextObject,
   polygonPalette: palette,
-  polygonSize: 50,
+  polygonSize: 10,
   gridSize: {
     width: plotWidth,
     height: plotHeight,
-  },
-  camera: {
-    x: 0,
-    y: 0
   },
   debugMode: {
     isEnable: true,
--- src/splot.ts
+++ src/splot.ts
@@ -409,6 +409,7 @@ export default class SPlot {
     'varying vec3 v_color;\n' +
     'void main() {\n' +
     '  gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0.0, 1.0);\n' +
+    '  gl_PointSize = 20.0;\n' +
     '  SET-VERTEX-COLOR-CODE' +
     '}\n'
 
@@ -1434,7 +1435,7 @@ export default class SPlot {
       this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.buffers.indexBuffers[i])
 
       // Рендеринг текущей группы буферов.
-      this.gl.drawElements(this.gl.TRIANGLES, this.buffers.amountOfGLVertices[i],
+      this.gl.drawElements(this.gl.POINTS, this.buffers.amountOfGLVertices[i],
         this.gl.UNSIGNED_SHORT, 0)
     }
   }
